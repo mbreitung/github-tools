8c84ba8f210b3366777efe16f585b2b6a3f5e0c4
Mark Breitung <mbreitung@pindrop.com>
2016-05-25 15:16:18 -0700

    initial commit

---
+++ pull_update_all.py
@@ -0,0 +1,94 @@
+import os
+import subprocess
+import sys
+
+import requests
+import requests.packages.urllib3
+import yaml
+
+requests.packages.urllib3.disable_warnings()
+
+
+def sync_fork(repo, repo_dir, github_user):
+    if 'parent' in repo:
+        try:
+            set_remote_upstream = 'git remote add upstream %s' % repo['parent']['ssh_url']
+            subprocess.check_output(set_remote_upstream, cwd=repo_dir, shell=True)
+        except:
+            pass
+
+        command_list = [
+            'git checkout master',
+            'git pull --rebase',
+        ]
+        if repo['owner']['login'] == github_user:
+            command_list.append('git push origin master')
+        for cmd in command_list:
+            try:
+                print subprocess.check_output(cmd, cwd=repo_dir, shell=True)
+            except Exception as e:
+                print 'failed to run command: %s' % cmd
+                raise e
+
+
+def update_repos(url, github_user, github_api_token, github_api_base_url, github_crt=None, is_team=True):
+    full_url = github_api_base_url + url
+    try:
+        auth = (github_user, github_api_token)
+        result = requests.get(full_url, auth=auth, verify=github_crt)
+        data = result.json()
+        if data and result.status_code < 300:
+            for repo in data:
+                print '--------------------------------------------------------------------------------------------'
+                print repo['ssh_url']
+                print repo['owner']['login']
+                pwd = subprocess.check_output('pwd').replace('\n', '')
+                base_dir = "%s/%s" % (pwd, repo['owner']['login'])
+                repo_dir = '%s/%s' % (base_dir, repo['name'])
+
+                if not os.path.exists(base_dir):
+                    os.mkdir(base_dir)
+
+                if not is_team:
+                    url = github_api_base_url + '/repos/%s/%s' % (repo['owner']['login'], repo['name'])
+                    repo_json = requests.get(url, auth=auth, verify=github_crt)
+
+                    try:
+                        try:
+                            sync_fork(repo_json.json(), repo_dir, github_user)
+                        except:
+                            pass
+                        print subprocess.check_output('git up', cwd=repo_dir, shell=True)
+                    except:
+                        cmd = 'git clone %s' % repo['ssh_url']
+                        print subprocess.check_output(cmd, cwd=base_dir, shell=True)
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc(file=sys.stdout)
+        print 'Failed to get api request from %s' % (full_url)
+        raise e
+
+
+def load_config():
+    with open(os.path.expanduser('~/.github-config'), 'r') as stream:
+        return yaml.safe_load(stream)
+
+
+if __name__ == '__main__':
+
+    config = load_config()
+    github_user = config['username']
+    github_api_token = config['api_token']
+    enterprise_host = config['enterprise_url']
+    github_crt = config['enterprise_cert']
+    github_api_base_url = 'https://%s/api/v3' % enterprise_host
+    teams = config['teams']
+    users = config['users']
+
+    for team in teams:
+        update_repos('/orgs/%s/repos' % team, github_user, github_api_token, github_api_base_url, github_crt)
+
+    for user in users:
+        update_repos('/users/%s/repos' % user, github_user, github_api_token,
+                     github_api_base_url, github_crt, is_team=False)
